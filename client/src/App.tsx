d9d58975cab8abdd903b3f4103de0c1a86573cc4
Nithin S Varrier <nithin486@hotmail.com>
2024-06-29 14:21:54 +0000

    feat: add server side logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import Board from "./Components/Board";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Game() {
+  // function handlePlay(nextSquares) {
+  //   setCurrentMove(nextHistory.length - 1);
+  // }
 
+  const [socketUrl, setSocketUrl] = useState("ws://localhost:3000");
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  const startGame = useCallback(() => {
+    sendMessage("Start"), setIsGameStarted(true);
+  }, []);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
+  const currentState = JSON.parse(lastMessage?.data || "{}");
+  console.log(currentState);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="game">
+      <button onClick={startGame}>Start Game</button>
+      <div className="game-board">
+        {currentState?.["state"] && (
+          <Board
+            isWinner={currentState["winner"]}
+            xIsNext={currentState["user"] === "X"}
+            squares={currentState["state"]}
+            onPlay={(val) => {
+              sendMessage(val.toString());
+            }}
+          />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
-const app = express();
+import expressWs from "express-ws";
+import { calculateWinner } from "./utils";
+const expressWsInstance = expressWs(express())
+const app = expressWsInstance.app;
 const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +13,52 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+var id = 0;
+
+let state = Array(9).fill(null);
+var aWss = expressWsInstance.getWss();
+
+
+function broadcast(data: Record<string, any>) {
+  aWss.clients.forEach(function (client) {
+    client.send(JSON.stringify(data));
+  });
+}
+
+app.ws('/', function (ws, req) {
+  if (id > 1) {
+    ws.send('{"error": "Game is full"}');
+    ws.close();
+    return;
+  }
+  const userid = id == 0 ? "X" : "O";
+  id++;
+
+
+  ws.on('message', function (msg) {
+    let data = {};
+    if (msg == 'Start') {
+      data = { user: userid, state: state, next: userid == "X" }
+      broadcast(data);
+      return;
+    }
+    state[parseInt(msg as unknown as string)] = userid;
+    if (calculateWinner(state)) {
+      data = { user: userid, state: state, winner: calculateWinner(state) }
+      broadcast(data);
+      aWss.clients.forEach(client => client.close());
+      return;
+    }
+    data = { user: userid, state: state }
+    broadcast(data);
+  });
+
+  ws.on('close', function () {
+    id = 0;
+    state = Array(9).fill(null);
+    broadcast({ user: "", state: state });
+  });
+  console.log('socket', userid);
+});
+
